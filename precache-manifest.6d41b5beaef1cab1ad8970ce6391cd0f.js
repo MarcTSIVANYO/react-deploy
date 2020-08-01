2cdea2fa6a9bf719c809c3ab4cbdb92a84dd5bfa
marcTSIVANYO <marctsivanyo@gmail.com>
2020-08-01 18:24:09 +0200

    Updates

---
+++ precache-manifest.6d41b5beaef1cab1ad8970ce6391cd0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43437bf0912c87b787247a763a367e1a",
+    "url": "/marctsivanyo.github.io/react-deploy/index.html"
+  },
+  {
+    "revision": "1643612888e3818922c9",
+    "url": "/marctsivanyo.github.io/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/marctsivanyo.github.io/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marctsivanyo.github.io/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1643612888e3818922c9",
+    "url": "/marctsivanyo.github.io/react-deploy/static/js/main.51f99c32.chunk.js"
+  },
+  {
+    "revision": "586142bdb42853b7ac85",
+    "url": "/marctsivanyo.github.io/react-deploy/static/js/runtime-main.27997176.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marctsivanyo.github.io/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
